79fd7a0fa49587bbcc4483baa73b6651d0393490
Digafe-1 <dzuriashwork@gmail.com>
2023-02-18 01:13:03 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - entry
- * return: Always 0
+ * Return: Always 0
  */
 int main(void)
 {
-	puts("\"programming is like building a multy lingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
