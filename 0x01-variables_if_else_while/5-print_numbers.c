c957121186a26aac1467ec422810d93070efc782
Digafe-1 <dzuriashwork@gmail.com>
2023-02-20 12:25:27 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Return single digit number
+ * i: variable
+ * Return: always zero
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
