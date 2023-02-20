3c8d85c3beba31812caa38298163170492fb70fa
Digafe-1 <dzuriashwork@gmail.com>
2023-02-20 12:48:55 -0800

    rivers

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Return lower case alphabet in revers
+ * ch: variable
+ * Return: always zero
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch <= 'a'; ch--)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
