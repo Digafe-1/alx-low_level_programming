ca97644e682feea93f92d2642e25828ee50f28b4
Digafe-1 <dzuriashwork@gmail.com>
2023-02-21 12:46:08 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * main - check the code.
- * Return: Always 0.
+ * print_alphabet_x10 -> print lower case alphabet
  */
 void print_alphabet_x10(void)
 {
 	int i;
 	int j;
-	
+
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
