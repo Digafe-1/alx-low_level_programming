cc22d53e6512cd258cd852fe485a8eb8fa7e6f08
Digafe-1 <dzuriashwork@gmail.com>
2023-02-21 12:27:53 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,9 +4,17 @@
  * main - check the code.
  * Return: Always 0.
  */
-
-int main(void)
+void print_alphabet_x10(void)
 {
-	print_alphabet_x10();
-	return (0);
+	int i;
+	int j;
+	
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
 }
