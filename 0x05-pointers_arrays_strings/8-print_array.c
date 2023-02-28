edd058f7bd596c0aea78c2ddd908cadb4504f38e
Digafe-1 <dzuriashwork@gmail.com>
2023-02-27 21:51:50 -0800

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,13 +7,10 @@
  * @n: Number of element of the array to be printed
  * Return: void
  */
-
 void print_array(int *a, int n)
-
 {
-
 	int j;
-
+	
 	for (j = 0; j < n; j++)
 	{
 		printf("%d", a[j]);
