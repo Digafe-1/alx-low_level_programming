c3c2b5eafcc29cee1de92816d2530abac9a1ce21
Digafe-1 <dzuriashwork@gmail.com>
2023-03-06 11:32:37 -0800

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main"
+#include "main.h"
 
 /**
  * _strchr - Entry point
@@ -16,5 +16,5 @@ char *_strchr(char *s, char c)
 		if (s[i] == c)
 			return (s + i);
 	}
-	return (null);
+	return (NULL);
 }
