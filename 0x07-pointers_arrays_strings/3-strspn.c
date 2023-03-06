ab7b544ccf9e963b7c0eb48399bb22f495d09a6e
Digafe-1 <dzuriashwork@gmail.com>
2023-03-06 12:15:00 -0800

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,12 +9,12 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, n, value, check;
-
+	
 	value = 0;
-
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		check = 0;
+		
 		for (n = 0; accept[n] != '\0'; n++)
 		{
 			if (accept[n] == s[i])
