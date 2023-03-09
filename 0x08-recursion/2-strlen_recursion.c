6d92efc98d14ea593492b0a31442c22e72dba4a8
Digafe-1 <dzuriashwork@gmail.com>
2023-03-09 10:45:26 -0800

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,15 +2,13 @@
 
 /**
  * _strlen_recursion - calculate the length of a string
- * @s: the string to count
- * @Return: integer value
+ * @s: string
+ * @Return: the length of a string
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-	{
 		return (0);
-	}
-	s++;
-	return (_strlen_recursion(s) + 1);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
