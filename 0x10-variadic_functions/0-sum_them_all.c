f231334451a96323d59b13643f9daf60ebe38019
Digafe-1 <dzuriashwork@gmail.com>
2023-03-23 12:38:21 -0700

    a function

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,11 +2,11 @@
 #include <stdarg.h>
 
 /**
- * sum_them_all - returns the sum of all its parameters
+ * sum_them_all - a function that returns the sum of all its parameters.
  * @n: amount of the argument
- * returns: sum of its parameters
+ * @...: the number of passed parameter
+ * Return: sum of its parameters
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list valist;
